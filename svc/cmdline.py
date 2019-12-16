80e097ce5d8287a5c0c55e18892b740131f944c0
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2019-12-16 03:19:58 +0200

    Add dockerfile and use it from travis

--- svc/cmdline.py
+++ svc/cmdline.py
@@ -26,7 +26,7 @@ class _Bootstrapper(object):
 
     def _run_app(self):
         self._application.run(
-            host=self._config.get("HOST", "localhost"),
+            host=self._config.get("HOST", "0.0.0.0"),
             port=self._config.get("PORT", 8080),
             debug=self._config.get('DEBUG', False),
         )
